4135e574fd28ff179726259b08af8d20a385a048
LockLight <Locklightz@gmail.com>
2017-01-15 21:36:51 +0800

    文件整合

---
+++ Alipay/Classes/Mine/View/LLMineBalanceCell.m
@@ -0,0 +1,18 @@
+//
+//  LLMineBalanceCell.m
+//  Alipay
+//
+//  Created by locklight on 17/1/15.
+//  Copyright © 2017年 LockLight. All rights reserved.
+//
+
+#import "LLMineBalanceCell.h"
+
+@implementation LLMineBalanceCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
